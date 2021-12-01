08ced21b776829c429585913586e23d2fad55919
Augustus Wilson <mcgusto88@gmail.com>
2021-12-01 12:15:07 -0500

    Initial Commit

---
+++ PracticeExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeExercise
+//
+//  Created by User on 12/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
